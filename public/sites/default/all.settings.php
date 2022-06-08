9f6c6a50e2e105a3bbe4a18fa07ec8d7bab7f8d0
Jussiles <juss.leskinen@gmail.com>
2022-06-08 13:33:04 +0300

    Tweak env and all.settings files.

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -8,12 +8,12 @@
 $settings['hash_salt'] = getenv('DRUPAL_HASH_SALT') ?: 'CHANGE-ME-IN-ENVIRONMENT-SETTINGS';
 
 if (getenv('ELASTIC_URL')) {
-  $config['elasticsearch_connector.cluster.infofinland']['url'] = getenv('ELASTIC_URL');
+  $config['elasticsearch_connector.cluster.tyollisyyspalvelut']['url'] = getenv('ELASTIC_URL');
 
   if (getenv('ELASTIC_USER') && getenv('ELASTIC_PASSWORD')) {
-    $config['elasticsearch_connector.cluster.infofinland']['options']['use_authentication'] = '1';
-    $config['elasticsearch_connector.cluster.infofinland']['options']['authentication_type'] = 'Basic';
-    $config['elasticsearch_connector.cluster.infofinland']['options']['username'] = getenv('ELASTIC_USER');
-    $config['elasticsearch_connector.cluster.infofinland']['options']['password'] = getenv('ELASTIC_PASSWORD');
+    $config['elasticsearch_connector.cluster.tyollisyyspalvelut']['options']['use_authentication'] = '1';
+    $config['elasticsearch_connector.cluster.tyollisyyspalvelut']['options']['authentication_type'] = 'Basic';
+    $config['elasticsearch_connector.cluster.tyollisyyspalvelut']['options']['username'] = getenv('ELASTIC_USER');
+    $config['elasticsearch_connector.cluster.tyollisyyspalvelut']['options']['password'] = getenv('ELASTIC_PASSWORD');
   }
 }
