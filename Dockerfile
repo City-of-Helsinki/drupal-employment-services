523fb7450c6e7f2d6c8d6674424b2db97f7f70a2
Maria Ahokas <maria.ahokas@siili.com>
2023-07-31 11:00:26 +0300

    THF-501: THF-501: snowball plugging

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Use the official Elasticsearch image as the base image
-FROM docker.elastic.co/elasticsearch/elasticsearch:7.17.6
-# Install the elasticsearch-analysis-raudikko plugin
-RUN bin/elasticsearch-plugin install https://github.com/EvidentSolutions/elasticsearch-analysis-raudikko/releases/download/v0.1.1/elasticsearch-analysis-raudikko-0.1.1-es7.17.6.zip
-# Expose the Elasticsearch port
-EXPOSE 9200
-EXPOSE 9300
-# Start Elasticsearch
-CMD ["bin/elasticsearch"]
--- public/modules/custom/tyollisyyspalvelut_elasticsearch/src/EventSubscriber/PrepareIndexMappingSubscriber.php
+++ public/modules/custom/tyollisyyspalvelut_elasticsearch/src/EventSubscriber/PrepareIndexMappingSubscriber.php
@@ -32,7 +32,6 @@ class PrepareIndexMappingSubscriber implements EventSubscriberInterface {
    */
   public function prepareIndexMapping(PrepareIndexMappingEvent $event) {
     $mappingParams = $event->getIndexMappingParams();
-
     if (isset($mappingParams['type'])) {
       $indexKey = $mappingParams['type'];
       foreach ($mappingParams['body'][$indexKey]['properties'] as $key => $property) {
@@ -44,7 +43,7 @@ class PrepareIndexMappingSubscriber implements EventSubscriberInterface {
     else {
       foreach ($mappingParams['body']['properties'] as $key => $property) {
         if ($property['type'] == 'text') {
-          $mappingParams['body']['properties'][$key]['analyzer'] = $mappingParams['body']['properties'][$key]['analyzer'] ?? 'standard';
+          $mappingParams['body']['properties'][$key]['analyzer'] = $mappingParams['body']['properties'][$key]['analyzer'] ?? 'index_analyzer';
           if ($key == 'title') {
             $mappingParams['body']['properties'][$key]['analyzer'] = 'default';
             unset($mappingParams['body']['properties'][$key]['search_analyzer']);
